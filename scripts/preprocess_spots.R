8cf6b19cdd7d2cad3766428d24458c6025bfe672
Taavi Päll <tapa741@gmail.com>
2020-09-24 15:41:25 +0300

    added sequencing metadata preprocessing script

---
+++ scripts/preprocess_spots.R
@@ -0,0 +1,47 @@
+library(readr)
+library(dplyr)
+library(stringr)
+library(here)
+
+spots_raw <- read_csv(here("data/spots.csv"), col_types = cols(tax_id = col_character())) %>% 
+  rename_all(str_to_lower)
+
+#' Fix missing columns
+columns_off <- spots_raw %>% 
+  filter(organism == "RNA-Seq")
+colnames(columns_off) <- c(setdiff(colnames(columns_off), "sample_name"), "sample_name")
+columns_off <- columns_off %>% 
+  mutate_at("spots", as.numeric) %>% 
+  mutate_at("tax_id", as.character)
+
+columns_ok <- spots_raw %>% 
+  filter(organism != "RNA-Seq")
+
+spots_col_fix <- bind_rows(columns_ok, columns_off)
+
+#' Fix cases with missing platform.
+#+
+seq_platform <- spots_col_fix %>% 
+  mutate_at(vars(platform, model), str_to_lower) %>% 
+  mutate(model = case_when(
+    is.na(model) & !is.na(platform) ~ platform,
+    TRUE ~ model
+  ),
+  model = case_when(
+    !str_detect(model, platform) ~ str_c(platform, model, sep = " "),
+    TRUE ~ model
+  ))
+
+spots <- seq_platform %>% 
+  group_by(geo_accession, library_strategy, library_source, library_selection, library_layout, platform, model, tax_id) %>% 
+  summarise_at("spots", list(reads = mean)) %>% 
+  ungroup()
+
+spots_unique_platform <- spots %>% 
+  group_by(Accession = geo_accession) %>% 
+  add_count() %>% 
+  ungroup() %>% 
+  filter(n == 1) %>% 
+  select(Accession, everything()) %>% 
+  select(-geo_accession, -n)
+write_csv(spots_unique_platform, here("output/spots_unique_platform.csv"))
