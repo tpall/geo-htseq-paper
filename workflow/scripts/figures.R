9891f45aec510d872a5ea8d72566a7f111f97f9b
Taavi Päll <tapa741@gmail.com>
2021-05-05 19:19:46 +0300

    merged GEO_articles.R to figures.R

--- workflow/scripts/figures.R
+++ workflow/scripts/figures.R
@@ -494,3 +494,168 @@ if (!exists("snakemake")) {
     geom_histogram(aes(prop_out)) +
     facet_wrap(~analysis_platform, scales = "free_y")
 }
+
+#' Figure 6
+#' Importing publication data.
+#+
+publications <- read_csv(
+  here("resources/data/publications.csv"),
+  col_types = cols(
+    .default = col_character(),
+    Id = col_character(),
+    HasAbstract = col_double(),
+    PmcRefCount = col_double()
+  ))
+pubs <- publications %>% 
+  mutate_at(c("ISSN", "ESSN"), str_remove_all, "-") %>% 
+  mutate(year = as.numeric(str_extract(PubDate, "^\\d{4}"))) %>% 
+  select(PubMedIds = Id, year, title = FullJournalName, ISSN, ESSN) %>% 
+  pivot_longer(cols = c("ISSN", "ESSN")) %>% 
+  select(-name) %>% 
+  drop_na()
+
+#' Importing citation data.
+#+
+citations <- read_csv(
+  here("resources/data/scopus_citedbycount.csv"),
+  col_types = cols(
+    PubMedIds = col_character(),
+    citations = col_double()
+  ))
+
+#' Import Citescore data.
+#+
+path <- here("resources/data/CiteScore 2011-2019 new methodology - October 2020.xlsx")
+citescore <- readxl::excel_sheets(path) %>% 
+  str_subset("^CiteScore")
+citescores_raw <- citescore %>% 
+  map(~readxl::read_xlsx(path, sheet = .x)) %>% 
+  set_names(citescore) %>% 
+  bind_rows(.id = "year")
+citescores <- citescores_raw %>% 
+  select(year, Title, CiteScore, ISSN = `Print ISSN`, ESSN = `E-ISSN`) %>% 
+  mutate_at("year", str_remove, "CiteScore ") %>% 
+  mutate_at("year", as.numeric) %>% 
+  pivot_longer(cols = c("ISSN", "ESSN")) %>% 
+  select(-name) %>% 
+  drop_na()
+
+#' Merge CiteScores with publication data.
+#+
+pubs_all <- pubs %>% 
+  left_join(citescores) %>% 
+  select(PubMedIds, year, Title, CiteScore) %>% 
+  distinct() %>% 
+  filter(year <= 2019)
+
+pubs_citescore <- pubs_all %>% 
+  drop_na() %>% 
+  left_join(citations)
+
+#' 
+#' Importing document summaries.
+#+
+document_summaries <- read_csv(
+  here("resources/data/document_summaries.csv"),
+  col_types = cols(
+    .default = col_character(),
+    Id = col_character(),
+    GDS = col_logical(),
+    GSE = col_double(),
+    ptechType = col_logical(),
+    valType = col_logical(),
+    SSInfo = col_logical(),
+    subsetInfo = col_logical(),
+    PDAT = col_date(format = ""),
+    n_samples = col_double(),
+    SeriesTitle = col_logical(),
+    PlatformTitle = col_logical(),
+    PlatformTaxa = col_logical(),
+    SamplesTaxa = col_logical()
+  ))
+
+doc_pubs <- document_summaries %>% 
+  select(Accession, PDAT, PubMedIds) %>% 
+  mutate(published = as.numeric(!is.na(PubMedIds)),
+         year = lubridate::year(PDAT)) %>% 
+  group_by(year) %>% 
+  summarise(published = sum(published), total = n())
+
+cbind(year = doc_pubs$year, Hmisc::binconf(doc_pubs$published, doc_pubs$total)) %>% 
+  as_tibble() %>% 
+  ggplot() +
+  geom_line(aes(year, PointEst)) +
+  geom_ribbon(aes(year, ymin = Lower, ymax = Upper), alpha = 0.3) +
+  scale_x_continuous(breaks = doc_pubs$year) +
+  labs(
+    x = "Year", 
+    y = "Proportion of GEO series with journal article",
+    caption = "Proportion of published GEO series that are related to journal publication. Gray area denotes binomial 95% confidence interval.")
+
+#' Add Accession to PubMedIds
+#+
+acc_pmid <- document_summaries %>% 
+  select(Accession, PubMedIds, taxon) %>% 
+  mutate(PubMedIds = map(PubMedIds, str_split, ";"),
+         PubMedIds = map(PubMedIds, 1)) %>% 
+  unnest(PubMedIds) %>% 
+  drop_na()
+
+data <- pvalues_sample %>% 
+  select(Accession, Class) %>% 
+  left_join(acc_pmid %>% 
+              left_join(pubs_citescore) %>% 
+              drop_na()) %>% 
+  drop_na() %>% 
+  mutate(anticons = as.numeric(Class == "anti-conservative"))
+
+f <- anticons ~ CiteScore + year
+family <- bernoulli()
+mod <- brm(
+  formula = f, 
+  data = data,
+  family = family, 
+  chains = chains, 
+  cores = cores, 
+  refresh = refresh,
+  iter = ifelse(is_ci(), 400, 2000),
+  control = list(adapt_delta = 0.99, max_treedepth = 12),
+  file = here("results/models/anticons__CiteScore_year.rds")
+)
+p <- plot(
+  conditional_effects(
+    mod, 
+    effects = "CiteScore"), 
+  line_args = list(color = "black"), plot = FALSE)
+pa <- p$CiteScore + 
+  scale_y_continuous(limits = c(0, 0.4)) +
+  labs(y = "Proportion of anti-conservative\np value histograms",
+       x = "Journal CiteScore")
+
+f <- anticons ~ log_citations + year
+log_citations <- data %>% 
+  mutate_at("citations", list(log_citations = ~log10(.x + 0.1)))
+mod <- brm(
+  formula = f, 
+  data = log_citations,
+  family = family, 
+  chains = chains, 
+  cores = cores, 
+  refresh = refresh,
+  iter = ifelse(is_ci(), 400, 2000),
+  control = list(adapt_delta = 0.99, max_treedepth = 12),
+  file = here("results/models/anticons__log_citations_year.rds")
+)
+p <- plot(
+  conditional_effects(
+    mod, 
+    effects = "log_citations"), 
+  line_args = list(color = "black"), plot = FALSE)
+pb <- p$log_citations +
+  scale_y_continuous(limits = c(0, 0.4)) +
+  scale_x_continuous(breaks = c(-1, 0, 1, 2, 3, 4), labels = c(0, 1, 10, 100, 1000, 1000)) +
+  labs(y = "Proportion of anti-conservative\np value histograms",
+       x = "Article citations")
+
+p <- pa + pb + plot_annotation(tag_levels = "A")
+ggsave(here("figures/figure_6.pdf"), height = 8, width = 12, dpi = 300, units = "cm")
